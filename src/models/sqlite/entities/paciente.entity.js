df0bb53a595a8a8543f5510ff97ae36a4dd1baec
Sergio Antozzi <santozzi@gmail.com>
2025-05-08 13:07:37 -0300

    listo por hoy

--- src/models/sqlite/entities/paciente.entity.js
+++ src/models/sqlite/entities/paciente.entity.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const {sequelize} = require('./../config/db.js');
-// Ruta absoluta al archivo
 
 const Paciente = sequelize.define('Paciente', {
   id: {
--- src/models/sqlite/paciente.model.js
+++ src/models/sqlite/paciente.model.js
@@ -1,12 +1,10 @@
 const {Paciente} = require('../sqlite/entities/paciente.entity.js');
 
-//refactorizar a conexion a base de datos
-// Sincronizar base de datos
-
   const getPacientesModel =  ()=>{
     const users = Paciente.findAll();
     return users;
   }
+//TODO: agregar operaciones CRUD
 
   module.exports = {
     getPacientesModel
