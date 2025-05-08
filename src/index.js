9fffb3b7f863a956734a885dfff07359a5b0a244
Sergio Antozzi <santozzi@gmail.com>
2025-05-08 13:01:07 -0300

    conexion base de datos

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ const Server = require('./server.js');
 const {connectDB} = require('./models/sqlite/config/db.js');
 
 
-connectDB().then(() => {
-  console.log('Base de datos conectada.');
-}).catch((error) => {
-  console.error('Error conectando a la base de datos:', error);
-});
+connectDB()
 const server = new Server("pug");
 
 server.listen();
